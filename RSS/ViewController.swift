059ee6a5aae9caf0281aa7605024da668edda827
Toph <toph.matta@gmail.com>
2015-07-30 21:20:07 -0500

    Upload RSS proj - Set nav controller

---
+++ RSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS
+//
+//  Created by Toph on 7/30/15.
+//  Copyright (c) 2015 Toph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
