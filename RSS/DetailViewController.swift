d43fea91ed1e54571811be83c1c7c50312863c44
Toph <toph.matta@gmail.com>
2015-07-30 21:47:33 -0500

    Created DetailViewController, setup transition from ViewController to DetailViewController using segue

--- RSS/DetailViewController.swift
+++ RSS/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  RSS
 //
 //  Created by Toph on 7/30/15.
@@ -8,14 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,9 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        <#code#>
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
-
